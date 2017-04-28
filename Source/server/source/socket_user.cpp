4b07790ada5545dac055c24234bfccedea55c445
Leon <tanzhi963@126.com>
2017-04-28 18:18:36 +0800

    daily commit

--- Source/server/source/socket_user.cpp
+++ Source/server/source/socket_user.cpp
@@ -1,8 +1,13 @@
-#include "socket_user.h"
+#include <iostream>
 
+#include "socket_user.h"
+#include "user.h"
+
+using namespace std;
 
 
 void* socket_user_thread(void *arg)
 {
-	
+	user *user_new = (user *)arg;
+	cout << user_new->get_m_iUser_socket_ID() << endl;
 }
